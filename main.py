6585c1df55b303ce26c08dee94a1296dd3164a22
jtorres <j.torres@maxcodex.com>
2023-12-13 09:05:31 -0400

    add db config

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from fastapi import FastAPI
 import uvicorn
+# from endpoints.gem_endpoints import gem_router
+# from endpoints.user_endpoints import user_router
+from models.gem_models import *
 
 app = FastAPI()
 
-@app.get('/')
-def home():
-    return 'Gemstone Shop with FastAPI'
+# app.include_router(gem_router)
+# app.include_router(user_router)
+
+# def create_db_and_tables():
+#     SQLModel.metadata.create_all(engine)
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0', port=8001)
+    uvicorn.run('main:app', host="0.0.0.0", port=8001, reload=True)
+    #create_db_and_tables()
