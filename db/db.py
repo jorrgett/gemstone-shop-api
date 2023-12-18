a21a0f37bcfdeb6435f3f4652a01c9257943ebe9
jtorres <j.torres@maxcodex.com>
2023-12-18 11:01:04 -0400

    change db and add alembic

--- db/db.py
+++ db/db.py
@@ -1,8 +1,8 @@
-from sqlmodel import create_engine
+from sqlalchemy import create_engine
 from sqlmodel import Session
 
 eng = 'database.db'
 
-sqlite_url = f'sqlite:///{eng}'
+sqlite_url = 'postgresql://gems_user:trOhcs6W67OARwm1rgyHWzCO2dQvBY1Z@dpg-clu784a1hbls73eb0f00-a.oregon-postgres.render.com/gems'
 engine = create_engine(sqlite_url, echo=True)
 session = Session(bind=engine)
